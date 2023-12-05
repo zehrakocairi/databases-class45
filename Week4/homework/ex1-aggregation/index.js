dbab400fdeb6929f778a83fa47b31dfde6215112
zehra <zehrakocairi@gmail.com>
2023-12-05 23:06:34 +0100

    complete second exercise

--- Week4/homework/ex1-aggregation/index.js
+++ Week4/homework/ex1-aggregation/index.js
@@ -2,17 +2,67 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const { seedDatabase } = require("./seedDatabase.js");
 
-async function deleteEpisodeExercise(client) {
-  /**
-   * It seems an errand episode has gotten into our data.
-   * This is episode 14 in season 31. Please remove it and verify that it has been removed!
-   */
+async function getCollection(client) {
+  return await client.db("databaseWeek4").collection("statistics");
+}
 
-  console.log(`Ran a command to delete episode and it deleted ${"TODO: fill in variable here"} episodes`);
+async function calculatePopulations(client, country) {
+  try {
+    const collection = await getCollection(client);
+    const query = [
+      { $match: { Country: country } },
+      {
+        $group: {
+          _id: "$Year",
+          countPopulation: { $sum: { $add: ["$M", "$F"] } },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          Year: "$_id",
+          countPopulation: 1,
+        },
+      },
+    ];
+
+    const result = await collection.aggregate(query).toArray();
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+}
+async function calculateContinentInfo(client, year, age) {
+  try {
+    const collection = await getCollection(client);
+    const query = [
+      { $match: { Year: year, Age: { $gte: age } } },
+      {
+        $group: {
+          _id: "$Continent",
+          TotalPopulation: { $sum: { $add: ["$M", "$F"] } },
+          Info: { $push: "$$ROOT" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          Continent: "$_id",
+          TotalPopulation: 1,
+          Info: 1,
+        },
+      },
+    ];
+
+    const result = await collection.aggregate(query).toArray();
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function main() {
-  let connectionString = "mongodb+srv://zehrakocairi:Test123...@cluster0.qzpvjfn.mongodb.net/";
+  let connectionString = "mongodb+srv://zehrakocairi:*******@cluster0.qzpvjfn.mongodb.net/";
 
   const client = new MongoClient(connectionString, {
     useNewUrlParser: true,
@@ -23,28 +73,16 @@ async function main() {
   try {
     await client.connect();
 
-    // Seed our database
     await seedDatabase(client);
+
+    await calculatePopulations(client, "Netherlands");
+
+    await getContinentInfo(client, 2020, "100+");
   } catch (err) {
     console.error(err);
   } finally {
-    // Always close the connection at the end
     client.close();
   }
 }
 
 main();
-
-/**
- * In the end the console should read something like this: 
-
-Created season 9 episode 13 and the document got the id 625e9addd11e82a59aa9ff93
-The title of episode 2 in season 2 is WINTER SUN
-The season and episode number of the "BLACK RIVER" episode is S02E06
-The episodes that Bob Ross painted a CLIFF are NIGHT LIGHT, EVENING SEASCAPE, SURF'S UP, CLIFFSIDE, BY THE SEA, DEEP WILDERNESS HOME, CRIMSON TIDE, GRACEFUL WATERFALL
-The episodes that Bob Ross painted a CLIFF and a LIGHTHOUSE are NIGHT LIGHT
-Ran a command to update episode 13 in season 30 and it updated 1 episodes
-Ran a command to update all the BUSHES to BUSH and it updated 120 episodes
-Ran a command to delete episode and it deleted 1 episodes
- 
-*/
--- Week4/homework/ex1-aggregation/seedDatabase.js
+++ Week4/homework/ex1-aggregation/seedDatabase.js
@@ -3,6 +3,9 @@ const records = require("./population_pyramid_1950-2022.json");
 const seedDatabase = async (client) => {
   const db = client.db("databaseWeek4");
   const collection = db.collection("statistics");
+  if ((await collection.findOne({})) != null) {
+    return;
+  }
 
   // Insert records into the database
   await collection.insertMany(records, (err, result) => {
